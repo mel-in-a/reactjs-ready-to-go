d1ea4a2c14eaf0bc704d68733edf51e14bf98b9e
Mélina <purple.melinaa@gmail.com>
2021-07-21 18:28:21 +0200

    description seo changée

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <Header />
+      <div class="container">
+        <div class="left">
+          <div class="line1">
+            <div class={switch1 ? 'btn-off' : 'btn-on'} onClick={() => setSwitch1(true)} >ON</div>
+            <div class={switch1 ? 'btn-on' : 'btn-off'} onClick={() =>setSwitch1(false)}>OFF</div>
+          </div>
+          <div class="line2">
+          <div class={switch2 ? 'btn-off' : 'btn-on'} onClick={() => setSwitch2(true)} >ON</div>
+            <div class={switch2 ? 'btn-on' : 'btn-off'} onClick={() =>setSwitch2(false)}>OFF</div>
+          </div>
+          <div class="line3">
+          <div class={switch3 ? 'btn-off' : 'btn-on'} onClick={() => setSwitch3(true)} >ON</div>
+            <div class={switch3 ? 'btn-on' : 'btn-off'} onClick={() =>setSwitch3(false)}>OFF</div>
+          </div>
+        </div>
+        <div class="right">
+          {switch1 && switch2 && switch3 ? <div class="btn-result bg-green" >Goooooo !</div>  :  <div class="btn-result bg-red" >No way !</div>}
+     
+        </div>
+      </div>
+<Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <i class="fas fa-space-shuttle fa-3x"></i>
+      </div>
+      <div className="title">Ready to go</div>
+    </div>
+  );
+};
+
+export default Header;
